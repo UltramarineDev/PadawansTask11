996048873888e10a423187a8bbaeb3fdf459dda2
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-02-20 17:03:04 +0300

    add solution

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,8 +6,36 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (accuracy >= 1 && accuracy <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+            double sumLeft = 0;
+            double sumRight = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    sumLeft += array[j];
+                }
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    sumRight += array[j];
+                }
+                if(Math.Abs(sumLeft - sumRight) <= accuracy)
+                {
+                    return i;
+                }
+                sumLeft = 0;
+                sumRight = 0;
+            }
+            return null;
+
         }
     }
 }
