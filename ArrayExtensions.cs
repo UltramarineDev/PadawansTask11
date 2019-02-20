beb6114e2ba7450fc9bdceee891365c81c2bea29
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-02-20 17:24:20 +0300

    add exceptions

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,17 +6,22 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            if (accuracy >= 1 && accuracy <= 0)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-
             if (array == null)
             {
                 throw new ArgumentNullException();
             }
+            if (array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            if (accuracy >= 1 || accuracy <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             double sumLeft = 0;
             double sumRight = 0;
+
             for (int i = 1; i < array.Length; i++)
             {
                 for (int j = 0; j < i; j++)
